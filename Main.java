3f10bd8a74a9a845b1d9b749a1f1fc7d058e5169
Andrewkamal <kamaa0880@wrdsb.ca>
2020-10-01 13:15:00 +0000

    fisned

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The example program for Nested stament
+ * @author  Andrew kamal
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create saner for user inout
+    Scanner input = new Scanner(System.in) ;
+    System.out.println("Does the animal have feather? (Yes/No");
+    String featherAnwser = input.nextline();
+
+    //check to see feather status
+    if(featherAnswer.equals ("yes")){
+      
+      System.out.println("what colour are the feathers?(red/blue");
+      String featherColour = input.nextline();
+
+      // detrime the brid
+      if(featherColour.equals("blue")) {
+          System.out.println( "That is a bluejay");
+
+      } else if ( featherColour.equals( "red")) {
+        System.out.println( "That is a cardianl");
+        //
+      }
     
+    // if the user answerd now 
+    }else{
+
+
+    }
   }
 }
